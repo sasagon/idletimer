29a93caa05118e6db241f16cc0f8e13937e97e21
SASAKI Hiroshi <sasagon@gmail.com>
2016-03-27 15:28:25 +0900

    modify run-test.sh not using stderr output of idletimer

--- src/test/run-test.sh
+++ src/test/run-test.sh
@@ -1,9 +1,7 @@
 #!/bin/sh --
-echo PWD: `pwd`
-echo '$0:' $0
 TESTDIR=`dirname $0`
 
-../idletimer -v -c "${TESTDIR}/run-test.config" > idletimer.log 2>&1 &
+../idletimer -v -c "${TESTDIR}/run-test.config" > idletimer.log 2> idletimer-error.log &
 IDLETIMER_PID=$!
 sleep 5
 kill $IDLETIMER_PID 2> /dev/null
@@ -13,10 +11,14 @@ cat <<EOT > idletimer.log.expected
   idle:1:echo "foo" > /dev/null
 *** Idletimer watching your operation...
 EOT
-if cmp idletimer.log.expected idletimer.log > /dev/null ; then
-  exit 0
-else
-  echo "Idletimer execution failed because of illegal log file as below:" 1>&2
+if !cmp idletimer.log.expected idletimer.log > /dev/null ; then
+  echo "Illegal execution log file found as below:" 1>&2
   cat idletimer.log 1>&2
   exit 1
+#elif [ -s idletimer-error.log ]; then
+#  echo "Idletimer error log found as below:" 1>&2
+#  cat idletimer-error.log 1>&2
+#  exit 1
 fi
+
+exit 0
